afd3cb6ddd74a96a2048c9addc066e64eff960aa
TJPo0KvneI4ljMIX <tjpo0kvnei4ljmix@gmail.com>
2021-09-13 18:17:18 +0000

    create starfinder class

---
+++ src/main/java/edu/brown/cs/student/main/StarFinder.java
@@ -0,0 +1,13 @@
+package edu.brown.cs.student.main;
+
+public class StarFinder {
+  /**
+   * Default constructor.
+   */
+  public StarFinder(/* Constructor should take a list of stars*/) {
+
+  }
+
+
+
+}
